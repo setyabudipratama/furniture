e697b0b39e9dfe23d99adbd702303db148a73489
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-10-02 10:48:58 +0700

    Update shop.js

--- shop.js
+++ shop.js
@@ -8,3 +8,27 @@ const utama = document.querySelector('.navbar .nav-item:first-child');
 utama.addEventListener('click', () => {
     window.location.href = "index.html";
 });
+
+// fungsi untuk beralih ke halaman shop orange sofa
+const orangeSofa = document.querySelectorAll('#orange .btn');
+orangeSofa.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href = "orange.html";
+    });
+});
+
+// fungsi untuk beralih ke halaman shop brown sofa
+const brownSofa = document.querySelectorAll('#brown .btn');
+brownSofa.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href = "brown.html";
+    });
+});
+
+// fungsi untuk beralih ke halaman shop white sofa
+const whiteSofa = document.querySelectorAll('#white .btn');
+whiteSofa.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href = "white.html";
+    });
+});
