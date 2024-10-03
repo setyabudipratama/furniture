aacab3a022d6c74da0a90cdd69bd30bd9f57b851
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-10-03 16:31:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-// fungsi untuk scroll ke arrivals
-const header = document.querySelector('header .fa-circle-down');
-header.addEventListener('click', () => {
-    const arrivals = document.querySelector('#arrivals');
-    arrivals.scrollIntoView({ behavior: 'smooth' });
+const btn = document.querySelector("header button");
+btn.addEventListener("click", () => {
+    const product = document.getElementById("orange");
+    product.scrollIntoView({ behavior: "smooth" });
 });
 
-// fungsi untuk scroll ke gallery
-const arrivals = document.querySelector('#arrivals .fa-circle-down');
-arrivals.addEventListener('click', () => {
-    const gallery = document.querySelector('#gallery');
-    gallery.scrollIntoView({ behavior: 'smooth' });
+// fungsi untuk beralih ke halaman utama
+const utama = document.querySelector('.navbar .nav-item:first-child');
+utama.addEventListener('click', () => {
+    window.location.href = "index.html";
 });
 
-// fungsi untuk scroll ke atas
-const gallery = document.querySelector('#gallery .fa-angles-up');
-gallery.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+// fungsi untuk beralih ke halaman shop orange sofa
+const orangeSofa = document.querySelectorAll('#orange .btn');
+orangeSofa.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href = "orange.html";
+    });
 });
 
-const shop = document.querySelector('.navbar .nav-item:nth-child(2)');
-shop.addEventListener('click', () => {
-    window.location.href="shop.html";
+// fungsi untuk beralih ke halaman shop brown sofa
+const brownSofa = document.querySelectorAll('#brown .btn');
+brownSofa.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href = "brown.html";
+    });
+});
+
+// fungsi untuk beralih ke halaman shop white sofa
+const whiteSofa = document.querySelectorAll('#white .btn');
+whiteSofa.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href = "white.html";
+    });
 });
