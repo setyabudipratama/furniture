253ccd12f904ec6c14b6bdaf897ecc43a1c0d0f1
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-10-03 19:48:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,16 @@ gallery.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+// fungsi untuk ke halaman shop 
 const shop = document.querySelector('.navbar .nav-item:nth-child(2)');
 shop.addEventListener('click', () => {
     window.location.href="shop.html";
+});
+
+// fungsi untuk beralih ke halaman shop
+const seeMore = document.querySelectorAll('#arrivals .btn');
+seeMore.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href="shop.html";
+    });
 });
