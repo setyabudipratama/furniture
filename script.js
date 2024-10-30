83ee64406c3b33c267db060a483db1e37c915311
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-10-30 19:55:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,53 @@ seeMore.forEach(btn => {
         window.location.href="shop.html";
     });
 });
+
+
+// rubah posisi button header
+const btnHeader = document.querySelector('header .fa-circle-down');
+
+// rubah tinggi viewport arrivals
+const viewportArrivals = document.getElementById('arrivals');
+
+// rubah tinggi template arrivals
+const sliderArrivals = document.querySelector('#arrivals .slider');
+
+// rubah footer
+const footer = document.querySelector('footer');
+Object.assign(footer.style, {
+    justifyContent: 'space-around',
+});
+
+// rubah ukuran h5 footer
+const h5Footer = document.querySelector('footer h5');
+
+// rubah ukuran span footer
+const spanFooter = document.querySelectorAll('footer span');
+
+function applyResponsiveStyles() {
+    if (window.innerWidth < 767) {
+        Object.assign(btnHeader.style, {
+            posision: 'absolute',
+            top: '500px',
+        });
+        viewportArrivals.style.height = '70vh';
+        sliderArrivals.style.top = '20%';
+        Object.assign(footer.style, {
+            justifyContent: 'center',
+            gap: '0',
+        });
+        h5Footer.style.fontSize = '0.6rem';
+        spanFooter.forEach(span => {
+            span.style.fontSize = '0.6rem';
+        });
+    } else if (window.innerWidth >= 767 && window.innerWidth < 1024) {
+        Object.assign(btnHeader.style, {
+            posision: 'absolute',
+            top: '500px',
+        })
+    }
+}
+
+// panggil fungsi
+applyResponsiveStyles();
+window.addEventListener('resize', applyResponsiveStyles);
