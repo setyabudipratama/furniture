59abbe586320197a52f21995db2bc57da3f97ea7
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-10-30 20:20:51 +0700

    Update shop.js

--- shop.js
+++ shop.js
@@ -32,3 +32,40 @@ whiteSofa.forEach(btn => {
         window.location.href = "white.html";
     });
 });
+
+
+// rubah footer
+const footer = document.querySelector('footer');
+Object.assign(footer.style, {
+    justifyContent: 'space-around',
+});
+
+// rubah ukuran h5 footer
+const h5Footer = document.querySelector('footer h5');
+
+// rubah ukuran span footer
+const spanFooter = document.querySelectorAll('footer span');
+
+
+// rubah ukuran teks h2
+const h2Teks = document.querySelectorAll('section h2');
+
+function applyResponsiveStyles() {
+    if (window.innerWidth < 767) {
+        h2Teks.forEach(h2 => {
+            h2.style.fontSize = '1.3rem';
+        });
+        Object.assign(footer.style, {
+            justifyContent: 'center',
+            gap: '0',
+        });
+        h5Footer.style.fontSize = '0.6rem';
+        spanFooter.forEach(span => {
+            span.style.fontSize = '0.6rem';
+        });
+    }
+}
+
+// panggil fungsi
+applyResponsiveStyles();
+window.addEventListener('resize', applyResponsiveStyles);
